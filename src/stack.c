564882eb1cae95c01155582e9d3f67ac270561a8
Mathius <info@yogimathius.dev>
2024-02-29 22:58:35 -0800

    chore: add push and pop opstack methods?

--- src/stack.c
+++ src/stack.c
@@ -1,6 +1,7 @@
 #include <stdbool.h>
 #include <limits.h>
 #include <stdlib.h>
+#include <my_bc.h>
 
 struct stack {
     int top;
@@ -46,3 +47,23 @@ int peek(struct stack *s){
     return s->array[s->top];
 }
 
+int nopstack=0;
+
+
+void push_opstack(struct operator_type *op, struct operator_type *opstack[MAX_OP_STACK])
+{
+  if(nopstack>MAX_OP_STACK-1) {
+    fprintf(stderr, "ERROR: Operator stack overflow\n");
+    exit(EXIT_FAILURE);
+  }
+  opstack[nopstack++]=op;
+}
+
+struct operator_type *pop_opstack(struct operator_type *opstack[MAX_OP_STACK])
+{
+  if(!nopstack) {
+    fprintf(stderr, "ERROR: Operator stack empty\n");
+    exit(EXIT_FAILURE);
+  }
+  return opstack[--nopstack];
+}
