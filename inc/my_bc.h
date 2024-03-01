9bf5da95107b63cca331a79442cb229dfe4b8af1
Mathius <info@yogimathius.dev>
2024-02-29 22:23:57 -0800

    chore: add operator_type and ASSOC enum

--- inc/my_bc.h
+++ inc/my_bc.h
@@ -21,6 +21,7 @@ struct queue {
     struct node *stack2;
 };
 
+
 void enqueue(struct queue *q, int x);
 void dequeue(struct queue *q);
 void display(const struct node *top1, const struct node *top2);
@@ -34,5 +35,25 @@ int _div(int a, int b);
 int _mod(int a, int b);
 int _add(int a, int b);
 int _sub(int a, int b);
+
+enum {ASSOC_NONE=0, ASSOC_LEFT, ASSOC_RIGHT};
+
+struct operator_type {
+  char op;
+  int prec;
+  int assoc;
+  int unary;
+  float (*eval)(float a1, float a2);
+} operators[]={
+  {'_', 10, ASSOC_RIGHT, 1, _uminus},
+  {'^', 9,  ASSOC_RIGHT, 0, _exp},
+  {'*', 8,  ASSOC_LEFT,  0, _mul},
+  {'/', 8,  ASSOC_LEFT,  0, _div},
+  {'%', 8,  ASSOC_LEFT,  0, _mod},
+  {'+', 5,  ASSOC_LEFT,  0, _add},
+  {'-', 5,  ASSOC_LEFT,  0, _sub},
+  {'(', 0,  ASSOC_NONE,  0, NULL},
+  {')', 0,  ASSOC_NONE,  0, NULL}
+};
 
 #endif
