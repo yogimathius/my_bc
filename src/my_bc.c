f270212690304d1eb139b4b63d9c291e4f303e18
Mathius <info@yogimathius.dev>
2024-03-20 00:09:10 -0700

    fix: handle more complex operations?

--- src/my_bc.c
+++ src/my_bc.c
@@ -17,28 +17,25 @@ int parse(char *tokens){
     }
     while (*tokens){
         if (*tokens >= '0' && *tokens <= '9') {
-            char *num = (char *)malloc(sizeof(char) * 10);
-            int i = 0;
-            num[i] = *tokens;
-            char *temp_ptr;
-            temp_ptr = tokens;
-            while (*temp_ptr++ && *temp_ptr >= '0' && *temp_ptr <= '9'){
-                i += 1;
-                num[i] = *temp_ptr;
+            int num = 0;
+            while (*tokens >= '0' && *tokens <= '9'){
+                num = num * 10 + (*tokens - '0');
+                tokens++;
             }
-            tokens += i;
-            enqueue(&q, atoi(num));
-        } else if (*tokens == '+' || *tokens == '-' || *tokens == '*' || *tokens == '/') {
+            enqueue(&q, num);
+            continue;
+        } else if (*tokens == '+' || *tokens == '-' || *tokens == '*' || *tokens == '/' || *tokens == '%' || *tokens == '^' || *tokens == '_') {
             struct operator_type *op = getop(*tokens);
             if (op == NULL){
                 dprintf(2, "ERROR: operator not found\n");
                 return EXIT_FAILURE;
             }
             if (q.operators[0] == NULL){
-                push_node(&q.stack2, *tokens);
+                push_opstack(op, q.operators, &total_operations);
             }
             else {
                 while (q.operators[total_operations - 1] && q.operators[total_operations - 1]->prec > op->prec){
+                    printf("shunting yard\n ");
                     struct operator_type *popped_operator = pop_opstack(q.operators, &total_operations);
 
                     push_node(&q.stack2, popped_operator->op);
@@ -70,13 +67,11 @@ int parse(char *tokens){
             struct operator_type *op = getop(pop_node(&q.stack2));
             int lhs = pop_node(&q.stack1);
             int rhs = pop_node(&q.stack1);
-            // printf("lhs: %c, rhs: %c\n", lhs, rhs);
 
-            
             int result = op->eval(lhs, rhs);
             // printf("result calculated: %d\n", result);
             // enqueue(&q, result);
-            push_to_bottom(&q.stack1, result);
+            push_node(&q.stack1, result);
             // display(q.stack1, q.stack2);
     }
     int result = q.stack1->data;
